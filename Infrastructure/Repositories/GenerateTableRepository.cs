520bec0e553d927a37558057f100fa006fa13336
Andhika Kurnia <kurniaandhika18@gmail.com>
2023-07-23 11:44:09 +0700

    - add animals and breed to generate table

--- Infrastructure/Repositories/GenerateTableRepository.cs
+++ Infrastructure/Repositories/GenerateTableRepository.cs
@@ -38,6 +38,8 @@ namespace Infrastructure.Repositories
                 typeof(SchemaVersion),
                 typeof(Profile),
                 typeof(Services),
+                typeof(Animals),
+                typeof(Breeds),
                 typeof(Owners),
                 typeof(Patients),
                 typeof(Products),
